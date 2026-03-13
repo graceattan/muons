eaecd3f1a198756d18cf795fb2b891f718479d77
g4ek <rath.s22@outlook.com>
2026-03-13 11:22:11 -0700

    added the results + removed unnecessary code

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ with open('lifetimes.txt', 'w') as file:
 
 with open(filename, 'r') as file:
 
-    current_state = "Detecting RE0"
-
     #1st element of list stores RE1_Time (Word 1)
     #2nd element of list stores RE1_Time (Word 2, Bits 0-4)
     #3rd element of list stores RE1_2_Time (Word 1)
     #4th element of list stores RE1_2_Time (Word 2, Bits
     #5th element of list stores muon lifetimes
+
     DecayTimes = ["","","","",""]
 
     lines = file.readlines() # list of all lines in file
@@ -23,25 +22,24 @@ with open(filename, 'r') as file:
     for i in range(len(lines)):
 
         if len(lines[i].split(" ")) != 16:
-            continue #skips lines that are not data
+            continue # skips lines that are not data
 
-        if get_bits(lines[i], 2, 7) == "1": #Trigger Event Detected, this resets all variables
-            current_state = "Detecting RE0"
+        if get_bits(lines[i], 2, 7) == "1": # trigger event detected, resets all variables
             DecayTimes = ["","","","",""]
-            # Coincidence Detected
+            # checks for coincidence
             if get_bits(lines[i], 2, 5) == "1" and get_bits(lines[i], 4, 5) == "1":
 
                 DecayTimes[0] = get_bits(lines[i], get_Time=True)
                 DecayTimes[1] = get_bits(lines[i], 2, 0, 5)
 
-                current_state = "Detecting RE1_2"
+                # Detecting RE1_2
                 for j in range(i+1, len(lines)):
                     if int(get_bits(lines[j], 1, length=8), 16) - int(DecayTimes[0], 16) > 5000: #if it's been more than 5ms since RE1, reset to look for next decay
                         break
                     # detects an electron from a muon decay, which means we can stop
                     # looking for RE1_2 and reset to look for next decay
                     if get_bits(lines[j], 4, 5) == "1" and get_bits(lines[j], 2, 5) == "0":
-                        current_state = "Decay Found"
+                        # found a decay
                         DecayTimes[2] = get_bits(lines[j], get_Time=True)
                         DecayTimes[3] = get_bits(lines[j], 4, 0, 5)
                         DecayTimes[4] = get_lifetime(DecayTimes[0], DecayTimes[1], DecayTimes[2], DecayTimes[3])
@@ -55,5 +53,4 @@ with open(filename, 'r') as file:
                                 break
 
                     # resets again to search for next decay
-                current_state = "Detecting RE0"
                 DecayTimes = ["", "", "", "", ""]
